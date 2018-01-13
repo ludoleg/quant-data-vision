da3c9d302b2a5792e254b7376143cadc55ca9871
ludo <ludo@ludos-mbp.attlocal.net>
2018-01-12 19:36:31 -0800

    Merge step 1

--- application/views.py
+++ application/views.py
@@ -180,7 +180,225 @@ def odr_demo():
     return render_template('odr_demo.html')
 
 
+@app.route('/odr_post')
+def odr_post():
+    return render_template('odr_post.html')
+
+
+# [START ODR service]
+# Duplicates /process with input from the ODR site
+@app.route('/chemin', methods=['GET', 'POST'])
+def chemin():
+    if request.method == 'POST':
+        # print request.__dict__
+        # Load data from request
+        # Ajax case
+        if request.is_json:
+            json_data = request.get_json()
+            data = json_data
+        # Regular post, text/plain encoded in body
+        else:
+            # Regular post x-www-form-urlencoded
+            dsample = request.form['data']
+            data = json.loads(dsample)
+            # Other type of encoding via text/plain
+            # a, b = request.data.split('=')
+            # data = json.loads(b)
+
+        sample = data['sample']
+        filename = sample['name']
+        array = sample['data']
+        app.logger.warning('Size of ODR array: %d', len(array))
+        print array
+
+        # Save to file
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'w') as outfile:
+            json.dump(array, outfile)
+
+        # Initialize the session object with chemin data
+        session['autoremove'] = False
+        session['dbname'] = 'difdata_CheMin.txt'
+        session['selected'] = phaselist.cheminPhases
+        session['available'] = phaselist.availablePhases
+        session['filename'] = filename
+
+        x = [li['x'] for li in array]
+        y = [li['y'] for li in array]
+
+        angle = np.asfarray(np.array(x))
+        diff = np.asfarray(np.array(y))
+
+        # Force mode
+        Lambda = 0.0
+        Target = 'Co'
+        FWHMa = 0.0
+        FWHMb = 0.35
+        InstrParams = {"Lambda": Lambda,
+                       "Target": Target,
+                       "FWHMa": FWHMa,
+                       "FWHMb": FWHMb}
+
+        # Parse phases sent by ODR
+        phasearray = data['phases']
+        selectedphases = [(d['name'], d['AMCSD_code']) for d in phasearray]
+        # TODO 2nd pass with selected
+
+        # Force Chemin for ODR
+        # Dif data captures all cristallographic data
+        # Load in the DB file
+        DBname = session['dbname']
+        difdata = open(DBname, 'r').readlines()
+        userData = (angle, diff)
+        results, BG, calcdiff = qxrd.Qanalyze(userData,
+                                              difdata,
+                                              selectedphases,
+                                              InstrParams,
+                                              session['autoremove'],
+                                              True)
+
+        # Re-create the subset of phases to select
+        sel, ava = rebalance(results)
+        session['selected'] = sel
+        session['available'] = ava
+        # print(twoT.tolist(), file=sys.stderr)
+        # print(userData, file=sys.stderr)
+
+        twoT = userData[0]
+        diff = userData[1]
+        angle = twoT
+        bgpoly = BG
+        xmin = 5
+        # xmax = max(angle)
+        Imax = max(diff[min(np.where(np.array(angle) > xmin)[0])
+                   :max(np.where(np.array(angle) > xmin)[0])])
+        offset = Imax / 2 * 3
+
+        Sum = calcdiff
+        difference_magnification = 1
+        difference = (diff - Sum) * difference_magnification
+        # logging.debug(results)
+        # logging.info("Done with processing")
+        difference = difference + offset
+
+        csv = 'ODR'
+        app.logger.warning('Length of angle array: %d', len(angle))
+
+        session['results'] = results
+        session['filename'] = filename
+
+        template_vars = {
+            'phaselist': results,
+            'angle': angle.tolist(),
+            'diff': diff.tolist(),
+            'bgpoly': bgpoly.tolist(),
+            'sum': calcdiff.tolist(),
+            'difference': difference.tolist(),
+            'url_text': csv,
+            'key': 'chemin',
+            'samplename': filename,
+            'mode': 'Chemin-ODR',
+            'availablephaselist': session['available'],
+            'selectedphaselist': session['selected']
+        }
+        return render_template('chart.html', **template_vars)
+    else:
+        return '''<html><body><h1>Did not get a post!</h1></body></html>'''
+# [END ODR service]
+
+# [START odr second pass process, this is run when the user changes the phases and relaunch after the chemin call]
+
+
+@app.route('/chemin_process', methods=['GET'])
+def chemin_process():
+    # Load parameters for computation
+    filename = session['filename']
+
+    # Extract angle, diff to populate userData
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as infile:
+        array = json.load(infile)
+    x = [li['x'] for li in array]
+    y = [li['y'] for li in array]
+    angle = np.asfarray(np.array(x))
+    diff = np.asfarray(np.array(y))
+
+    # Force mode
+    Lambda = 0.0
+    Target = 'Co'
+    FWHMa = 0.0
+    FWHMb = 0.35
+    InstrParams = {"Lambda": Lambda,
+                   "Target": Target,
+                   "FWHMa": FWHMa,
+                   "FWHMb": FWHMb}
+
+    # Phase selection
+    selectedPhases = session['selected']
+    # Dif data captures all cristallographic data
+    selectedphases = []
+    for i in range(len(selectedPhases)):
+        name, code = selectedPhases[i].split('\t')
+        code = int(code)
+        selectedphases.append((name, code))
+
+    # Load in the DB file
+    DBname = session['dbname']
+    difdata = open(DBname, 'r').readlines()
+    userData = (angle, diff)
+    results, BG, calcdiff = qxrd.Qanalyze(userData,
+                                          difdata,
+                                          selectedphases,
+                                          InstrParams,
+                                          session['autoremove'],
+                                          True)
+
+    # Re-create the subset of phases to select
+    sel, ava = rebalance(results)
+    session['selected'] = sel
+    session['available'] = ava
+    # print(twoT.tolist(), file=sys.stderr)
+    # print(userData, file=sys.stderr)
+
+    twoT = userData[0]
+    diff = userData[1]
+    angle = twoT
+    bgpoly = BG
+    xmin = 5
+    # xmax = max(angle)
+    Imax = max(diff[min(np.where(np.array(angle) > xmin)[0])                    :max(np.where(np.array(angle) > xmin)[0])])
+    offset = Imax / 2 * 3
+
+    Sum = calcdiff
+    difference_magnification = 1
+    difference = (diff - Sum) * difference_magnification
+    # logging.debug(results)
+    # logging.info("Done with processing")
+    difference = difference + offset
+
+    csv = 'ODR'
+    app.logger.warning('Length of angle array: %d', len(angle))
+
+    session['results'] = results
+
+    template_vars = {
+        'phaselist': results,
+        'angle': angle.tolist(),
+        'diff': diff.tolist(),
+        'bgpoly': bgpoly.tolist(),
+        'sum': calcdiff.tolist(),
+        'difference': difference.tolist(),
+        'url_text': csv,
+        'key': 'ludo',
+        'samplename': filename,
+        'mode': session['dbname'],
+        'availablephaselist': session['available'],
+        'selectedphaselist': session['selected']
+    }
+    return render_template('chart.html', **template_vars)
+# [END process]
+
 # [START process]
+
+
 @app.route('/process', methods=['GET', 'POST'])
 def process():
     if request.method == 'GET':
