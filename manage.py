c20598153bf7b9f5ce1df4f4ab1da2025ba5b24b
ludo <ludo@ludos-mbp.attlocal.net>
2018-01-23 00:01:28 -0800

    Removing coverage as it is breaking the deployment on staging on Heroku (?)

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 import unittest
-import coverage
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import os
@@ -18,24 +17,6 @@ def test():
     """Runs the unit tests without coverage."""
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-@manager.command
-def cov():
-    """Runs the unit tests with coverage."""
-    cov = coverage.coverage(
-        branch=True, include='application/*', omit='*/__init__.py')
-    cov.start()
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    cov.stop()
-    cov.save()
-    print 'Coverage Summary:'
-    cov.report()
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    covdir = os.path.join(basedir, 'coverage')
-    cov.html_report(directory=covdir)
-    cov.erase()
 
 
 if __name__ == '__main__':
